7f2bd78b8a6f233facdb2b366a3585d43eafb0b5
Andriy Tomkevych <91atom@gmail.com>
2016-11-05 12:00:56 +0000

    sd

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// JavaScript File
+//alert('hello');
+//console.log('hello');
+//document.write('Hello');
+
+/*var sum = 1991 + 4 + 26;
+var text = 'My magic number:' ;
+var result = text - sum;
+document.write(result);
+*/
+/*var year = Number(prompt('Input year'));
+var month = Number(prompt('Input month'));
+var day = Number(prompt('Inout day'));
+var sum = year + month + day;
+var text = 'Your magic number: ';
+var result = text + sum;
+document.write(result);*/
+
+/*var day = 1;
+var year = 2;
+var month = 10;
+var sum = sumArgs(year, month, day);
+var text = 'Your magic number: ';
+var result = text + sum;
+document.write(result);
+
+function sumArgs(a, b, c) {
+    var result = a + b + c ;
+    return result;
+}
+*/
+/*function tellAge(age) {
+    if (age < 18) {
+        return 'You are kid';
+    }
+    else {
+        return 'You are adult'
+    }
+}
+var age = Number(prompt('Tell your age'));
+var result = tellAge(age);
+document.write(result);
+*/
+result = Math.sqrt(25);
+document.write(result);
